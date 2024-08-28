caa29297fa66651a44ac072ea506899400c2cdd2
yashmayani <yashmayani522@gmail.com>
2024-08-28 17:28:03 +0530

    - Remove unwanted space in config file.

--- config.php
+++ config.php
@@ -1,20 +1,13 @@
 <?php
-
 $host="localhost";
 $username="root";
 $password=null;
 $database="prodev_infotech";
-
 
 $conn=new mysqli ($host,$username,$password,$database);
 if($conn->connect_error){
     die("some error".$conn->$connect_error);
    
 }
-
-
-
-
-
 
 ?>
